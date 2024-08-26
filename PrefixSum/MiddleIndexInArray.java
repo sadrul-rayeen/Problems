72d63999e3035472d3c0d0f02af31ef2dfc56887
Adil <adil@Adils-MacBook-Air.local>
2024-08-26 06:56:10 +0530

    Prefix sum

---
+++ PrefixSum/MiddleIndexInArray.java
@@ -0,0 +1,33 @@
+package PrefixSum;
+
+public class MiddleIndexInArray {
+    public static int findMiddleIndex(int[] arr) {
+        int n = arr.length;
+        int leftSum = 0;
+        int total = 0;
+
+        for (int i = 0; i < n; i++) {
+            total += arr[i];
+        }
+
+        for (int j = 0; j < arr.length; j++) {
+            int rightSum = total - leftSum - arr[j];
+
+            if (leftSum == rightSum) {
+                return j;
+            }
+
+            leftSum += arr[j];
+        }
+
+        return -1;
+
+    }
+    public static void main(String[] args) {
+        int[] arr = { 2, 3, -1, 8, 4 };
+
+        int result = findMiddleIndex(arr);
+
+        System.out.println(result);
+    }
+}
