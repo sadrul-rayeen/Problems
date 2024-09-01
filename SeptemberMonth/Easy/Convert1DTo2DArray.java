cfbfa9632f26ebfb8c503421882d18c83ae25055
Adil <adil@Adils-MacBook-Air.local>
2024-09-01 13:59:32 +0530

    optimize

--- SeptemberMonth/Easy/Convert1DTo2DArray.java
+++ SeptemberMonth/Easy/Convert1DTo2DArray.java
@@ -20,12 +20,28 @@ public class Convert1DTo2DArray {
 
         return result;
     }
+    public static int[][] convert1DTo2DOptimal(int[] original, int m, int n) {
+        int size = original.length;
+
+        if (m * n != size) {
+            return new int[0][0];
+        }
+
+        int result[][] = new int[m][n];
+
+        for (int i = 0; i < size; i++) {
+            result[i / n][i % n] = original[i];
+        }
+
+        return result;
+    }
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4 };
         int m = 2;
         int n = 2;
 
-        convert1DTo2D(arr, m, n);
+        // convert1DTo2D(arr, m, n);
+        convert1DTo2DOptimal(arr, m, n);
 
     }
 }
