f388fbcd6d012ce0aa02e9c232ce5817cb6dd2f8
Adil <adil@Adils-MacBook-Air.local>
2024-10-09 08:27:03 +0530

    remove spaces

--- MergeIntervals/IntervalListIntersection.java
+++ MergeIntervals/IntervalListIntersection.java
@@ -5,17 +5,12 @@ import java.util.List;
 
 public class IntervalListIntersection {
     public static int[][] intersectionList(int[][] a, int[][] b) {
-        System.out.println(a.length);
-        System.out.println(b.length);
-
-        List<int[]> result = new ArrayList<>();
+         List<int[]> result = new ArrayList<>();
 
         int aPointer = 0;
         int bPointer = 0;
         int sizeA = a.length;
         int sizeB = b.length;
-
-        
 
         while (aPointer < sizeA && bPointer < sizeB) {
             if (b[bPointer][0] <= a[aPointer][1] && a[aPointer][0] <= b[bPointer][1]) {
@@ -23,10 +18,8 @@ public class IntervalListIntersection {
 
                 temp[0] = Math.max(a[aPointer][0], b[bPointer][0]);
                 temp[1] = Math.min(a[aPointer][1], b[bPointer][1]);
-                
-                result.add(temp);
 
-                System.out.println(temp[0] + " " + temp[1]);
+                result.add(temp);
             }
 
             if (a[aPointer][1] > b[bPointer][1]) {
@@ -37,7 +30,6 @@ public class IntervalListIntersection {
         }
 
         return result.toArray(new int[result.size()][]);
-
     }
     public static void main(String[] args) {
         int[][] firstList = { { 0, 2 }, { 5, 10 }, { 13, 23 }, { 24, 25 } };
