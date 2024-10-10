9107294572e5460fbf8892fcc941634f215dc850
Sadrul <156033559+sadrul-rayeen@users.noreply.github.com>
2024-10-10 21:04:17 +0530

    Update IntervalListIntersection.java

--- MergeIntervals/IntervalListIntersection.java
+++ MergeIntervals/IntervalListIntersection.java
@@ -33,7 +33,7 @@ public class IntervalListIntersection {
     }
     public static void main(String[] args) {
         int[][] firstList = { { 0, 2 }, { 5, 10 }, { 13, 23 }, { 24, 25 } };
-        int[][] secondList = { { 1, 5 }, { 8, 12 }, { 15, 24 }, { 25, 26 } };
+        int[][] secondList = { { 1, 5 }, { 8, 12 }, { 15, 24 }, { 25, 27 } };
 
         int[][] result = intersectionList(firstList, secondList);
 
